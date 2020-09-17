938d9e9c65b7f7e7127a4e1995464ed31d9cc398
Rexagon <reide740@gmail.com>
2020-09-17 09:35:42 +0300

    Add gate open params

--- wallet/wallet_window.cpp
+++ wallet/wallet_window.cpp
@@ -422,7 +422,7 @@ void Window::showAccount(const QByteArray &publicKey, bool justCreated) {
 	data.updateDecrypted = _decrypted.events();
 	data.transitionEvents = _infoTransitions.events();
 	data.share = shareAddressCallback();
-	data.openGate = [this]() { _wallet->openGate(); };
+	data.openGate = [this]() { _wallet->openGate(_rawAddress); };
 	data.useTestNetwork = _wallet->settings().useTestNetwork;
 	_info = std::make_unique<Info>(_window->body(), std::move(data));
 
